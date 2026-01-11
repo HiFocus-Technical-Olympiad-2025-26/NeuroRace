a813b8e56a9e72eb0428512289f6eab14f1f3ec9
TomasKruta <tomas.kruta@outlook.cz>
2026-01-11 21:52:04 +0100

    implemented fade in logo

--- Assets/Scripts/BootLoader.cs
+++ Assets/Scripts/BootLoader.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class BootLoader : MonoBehaviour
 {
-    [SerializeField] private float delay = 1f; // time for fade/logo
     [SerializeField] private string sceneName = "FirstMenu";
+    [SerializeField] private CanvasGroup fadeGroup;
+    [SerializeField] private float delayBeforeInFade = 1f;
+    [SerializeField] private float fadeDuration = 2f;
+    [SerializeField] private float delay = 1.5f; // time for fade/logo
+    [SerializeField] private float delayAfterOutFade = 0.5f;
 
     private void Start()
     {
@@ -16,7 +20,32 @@ public class BootLoader : MonoBehaviour
 
     private IEnumerator LoadScene()
     {
+        yield return new WaitForSeconds(delayBeforeInFade);
+
+        // Fade in
+        yield return Fade(0f, 1f);
+
         yield return new WaitForSeconds(delay);
+
+        // Fade out
+        yield return Fade(1f, 0f);
+
+        yield return new WaitForSeconds(delayAfterOutFade);
+
         SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator Fade(float from, float to)
+    {
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            fadeGroup.alpha = Mathf.Lerp(from, to, t / fadeDuration);
+            yield return null;
+        }
+
+        fadeGroup.alpha = to;
     }
 }
